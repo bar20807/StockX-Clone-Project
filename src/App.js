f2722c938da1326b1344a1190d978cf41aa3b630
bar20807@uvg.edu.gt <Lolo1234>
2022-05-02 00:32:30 -0600

    Header-Elements

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      </div>)
+}
+export default App;
