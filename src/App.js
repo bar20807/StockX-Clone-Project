d7fe72a13a25205b1050c1dd133b585d43312e9e
bar20807@uvg.edu.gt <Lolo1234>
2022-05-03 01:02:44 -0600

    Navbar Categories agregada

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import Header from './Header';
+import Body from './Body';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Body />
       </div>)
 }
 export default App;
---
+++ src/Body.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './style.css';
+
+function Categories() {
+    return (
+            <section className="categories">
+                <ul>
+                    <li>
+                        Sneakers
+                    </li>
+                    <li>
+                        Apparel
+                    </li>
+                    <li>
+                        Electronics
+                    </li>
+                    <li>
+                        Trading Cards
+                    </li>
+                    <li>
+                        Collectibles
+                    </li>
+                    <li>
+                        Accesories
+                    </li>
+                    <li>
+                        NFTs
+                    </li>
+                </ul>
+            </section>
+    )
+}
+
+export default Categories;
